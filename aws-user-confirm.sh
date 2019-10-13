fe22254097a6c20f69953b3b35526409a927b28a
Pavel Struhar <pstruhar@gmail.com>
2019-10-13 19:24:34 +0200

    Add helper scripts

---
+++ aws-user-confirm.sh
@@ -0,0 +1,4 @@
+aws cognito-idp admin-confirm-sign-up \
+  --region YOUR_COGNITO_REGION \
+  --user-pool-id YOUR_COGNITO_USER_POOL_ID \
+  --username admin@example.com
---
+++ aws-user-signup.sh
@@ -0,0 +1,5 @@
+aws cognito-idp sign-up \
+  --region YOUR_COGNITO_REGION \
+  --client-id YOUR_COGNITO_APP_CLIENT_ID \
+  --username admin@example.com \
+  --password Passw0rd!
